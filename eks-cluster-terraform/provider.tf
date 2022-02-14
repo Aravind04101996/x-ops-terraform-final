32270c802283268c27ac63ed01ad3c32a04c128d
Aravind <aravindcse201820@gmail.com>
2022-02-14 10:08:43 +0530

    removed duplicate provider

--- eks-cluster-terraform/provider.tf
+++ eks-cluster-terraform/provider.tf
@@ -8,12 +8,3 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "< 4.0"
-    }
-  }
-}
